a5b7512228f6f37a2227a95211f6e59e47f83528
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2023-10-20 10:42:30 +0300

    fix

--- src/js/components/bankOffer.js
+++ src/js/components/bankOffer.js
@@ -12,7 +12,6 @@ const bankOffer = () => {
         const monthPayment = infoItems[1].querySelector('div > span');
         const term = infoItems[2].querySelector('div > span');
         const sum = infoItems[3].querySelector('div > span');
-        const overpayment = infoItems[4].querySelector('div > span');
 
         if (choiceContainer) {
             choiceContainer.addEventListener('click', (e) => {
@@ -42,31 +41,24 @@ const bankOffer = () => {
                     const choiceItems = choiceContainer.querySelectorAll('.bank-offer__choice-item');
                     if (choiceItem === choiceItems[0]) {
                         monthPayment.textContent = '33 932 ₽';
-                        overpayment.textContent = '9 565 513 ₽';
                     }
                     if (choiceItem === choiceItems[1]) {
                         monthPayment.textContent = '32 873 ₽';
-                        overpayment.textContent = '9 184 255 ₽';
                     }
                     if (choiceItem === choiceItems[2]) {
                         monthPayment.textContent = '31 819 ₽';
-                        overpayment.textContent = '8 804 865 ₽';
                     }
                     if (choiceItem === choiceItems[3]) {
                         monthPayment.textContent = '30 771 ₽';
-                        overpayment.textContent = '8 427 538 ₽';
                     }
                     if (choiceItem === choiceItems[4]) {
                         monthPayment.textContent = '29 729 ₽';
-                        overpayment.textContent = '8 052 483 ₽';
                     }
                     if (choiceItem === choiceItems[5]) {
                         monthPayment.textContent = '28 694 ₽';
-                        overpayment.textContent = '7 679 916 ₽';
                     }
                     if (choiceItem === choiceItems[6]) {
                         monthPayment.textContent = '27 667 ₽';
-                        overpayment.textContent = '7 310 077 ₽';
                     }
                 }
             })

51e0a793cd13d4bf155a82e33e068386c0764b55
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2023-10-21 11:21:05 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -11997,11 +11997,20 @@ __webpack_require__.r(__webpack_exports__);
 const dropdownDown = (textSelector, targetSelector) => {
   const items = document.querySelectorAll(textSelector);
   if (!items.length >= 1) return;
+  const mapBtn = {
+    more: 'Показать полностью',
+    hide: 'Скрыть'
+  };
   items.forEach(text => {
     const btn = text.nextElementSibling;
     btn.addEventListener('click', () => {
-      text.classList.add('_active');
-      btn.remove();
+      if (!text.classList.contains('_active')) {
+        text.classList.add('_active');
+        btn.querySelector('span').textContent = mapBtn.hide;
+      } else {
+        text.classList.remove('_active');
+        btn.querySelector('span').textContent = mapBtn.more;
+      }
     });
   });
 };
--- src/js/modules/dropdownDown.js
+++ src/js/modules/dropdownDown.js
@@ -1,11 +1,21 @@
 const dropdownDown = (textSelector, targetSelector) => {
     const items = document.querySelectorAll(textSelector);
     if (!items.length >= 1) return;
+
+    const mapBtn = {
+        more: 'Показать полностью',
+        hide: 'Скрыть'
+    }
     items.forEach(text => {
         const btn = text.nextElementSibling;
         btn.addEventListener('click', () => {
-            text.classList.add('_active');
-            btn.remove();
+            if (!text.classList.contains('_active')) {
+                text.classList.add('_active');
+                btn.querySelector('span').textContent = mapBtn.hide;
+            } else {
+                text.classList.remove('_active');
+                btn.querySelector('span').textContent = mapBtn.more;
+            }
         })
     });
 };

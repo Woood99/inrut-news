04a44381bded2183f3bcf6bc7641696e8bbef1b9
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2023-12-12 16:42:29 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -13345,16 +13345,24 @@ const tooltipSecondary = () => {
   items.forEach(item => {
     const btn = item.querySelector('.secondary-tooltip__btn');
     const btnClose = item.querySelector('.secondary-tooltip__close');
-    btn.addEventListener('click', () => {
+    btn.addEventListener('click', e => {
+      e.preventDefault();
+      items.forEach(currentItem => {
+        if (item !== currentItem) {
+          currentItem.classList.remove('_active');
+        }
+      });
       if (!item.classList.contains('_active')) {
         item.classList.add('_active');
       } else {
         item.classList.remove('_active');
       }
     });
-    btnClose.addEventListener('click', () => {
-      item.classList.remove('_active');
-    });
+    if (btnClose) {
+      btnClose.addEventListener('click', () => {
+        item.classList.remove('_active');
+      });
+    }
   });
   document.addEventListener('click', e => {
     const target = e.target;
--- src/js/components/tooltips.js
+++ src/js/components/tooltips.js
@@ -3,16 +3,24 @@ export const tooltipSecondary = () => {
     items.forEach(item => {
         const btn = item.querySelector('.secondary-tooltip__btn');
         const btnClose = item.querySelector('.secondary-tooltip__close');
-        btn.addEventListener('click', () => {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            items.forEach(currentItem => {
+                if (item !== currentItem) {
+                    currentItem.classList.remove('_active')
+                }
+            });
             if (!item.classList.contains('_active')) {
                 item.classList.add('_active');
             } else {
                 item.classList.remove('_active');
             }
         });
-        btnClose.addEventListener('click',() => {
-            item.classList.remove('_active');
-        })
+        if (btnClose) {
+            btnClose.addEventListener('click',() => {
+                item.classList.remove('_active');
+            })
+        }
     });
     document.addEventListener('click',(e) => {
         const target = e.target;

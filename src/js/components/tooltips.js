cd90f0f3d9d09ae4fe221d5d723eb5690671559c
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2024-02-01 12:49:10 +0300

    fix

--- src/js/components/tooltips.js
+++ src/js/components/tooltips.js
@@ -7,41 +7,43 @@ export const tooltipSecondary = () => {
         const btnClose = item.querySelector('.secondary-tooltip__close');
         const content = item.querySelector('.secondary-tooltip__content')
         btn.addEventListener('click', (e) => {
-            e.preventDefault();
-            items.forEach(currentItem => {
-                if (item !== currentItem) {
-                    currentItem.classList.remove('_active')
+            if (!item.classList.contains('secondary-tooltip--link')) {
+                e.preventDefault();
+                items.forEach(currentItem => {
+                    if (item !== currentItem) {
+                        currentItem.classList.remove('_active')
+                    }
+                });
+                if (!item.classList.contains('_active')) {
+                    item.classList.add('_active');
+                } else {
+                    item.classList.remove('_active');
                 }
-            });
-            if (!item.classList.contains('_active')) {
-                item.classList.add('_active');
-            } else {
-                item.classList.remove('_active');
-            }
-
-            const cardPrice = item.closest('.card-price');
-            if (cardPrice && window.innerWidth <= 1212) {
-                let status = '';
-                if (cardPrice.classList.contains('card-price--down')) status = '_down';
-                if (cardPrice.classList.contains('card-price--up')) status = '_up';
-                const modalHTML = `
-                <div class="tooltip-modal">
-                    <div class="tooltip-modal__container">
-                        <button class="btn-reset tooltip-modal__close" aria-label="Закрыть модальное окно">
-                            <svg>
-                                <use xlink:href="./img/sprite.svg#x"></use>
-                            </svg>
-                            <span>Закрыть</span>
-                        </button>
-                        <div class="tooltip-modal__content">
+    
+                const cardPrice = item.closest('.card-price');
+                if (cardPrice && window.innerWidth <= 1212) {
+                    let status = '';
+                    if (cardPrice.classList.contains('card-price--down')) status = '_down';
+                    if (cardPrice.classList.contains('card-price--up')) status = '_up';
+                    const modalHTML = `
+                    <div class="tooltip-modal">
+                        <div class="tooltip-modal__container">
+                            <button class="btn-reset tooltip-modal__close" aria-label="Закрыть модальное окно">
+                                <svg>
+                                    <use xlink:href="./img/sprite.svg#x"></use>
+                                </svg>
+                                <span>Закрыть</span>
+                            </button>
+                            <div class="tooltip-modal__content">
+                            </div>
                         </div>
                     </div>
-                </div>
-                `;
-                modal(modalHTML, '.tooltip-modal', 300, item);
-                const tooltipModal = document.querySelector('.tooltip-modal');
-                if (status) tooltipModal.classList.add(status);
-                tooltipModal.querySelector('.tooltip-modal__content').innerHTML = content.outerHTML;
+                    `;
+                    modal(modalHTML, '.tooltip-modal', 300, item);
+                    const tooltipModal = document.querySelector('.tooltip-modal');
+                    if (status) tooltipModal.classList.add(status);
+                    tooltipModal.querySelector('.tooltip-modal__content').innerHTML = content.outerHTML;
+                }
             }
         });
         if (btnClose) {

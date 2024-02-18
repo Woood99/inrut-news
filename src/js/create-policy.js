471a9e2bd17bd57740336037b900a3d40c4e766a
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2024-02-18 10:37:47 +0300

    fix

--- src/js/create-policy.js
+++ src/js/create-policy.js
@@ -21,8 +21,7 @@ import {
 import {
     validateTextMap
 } from './modules/validateTextMap';
-import { getCurrentTime } from './modules/date';
-import dataFaq from './data/dataFaq';
+
 // ==============================
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -53,7 +52,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
     const form = document.querySelector('.create-policy__form');
-    const faq = document.querySelector('.create-policy__faq');
     if (form) {
         const checkbox = form.querySelector('.create-policy__checkbox');
         const btn = form.querySelector('.create-policy__btn');
@@ -186,66 +184,6 @@ document.addEventListener('DOMContentLoaded', () => {
             if (itemsError.length === 0) return;
             const firstItem = itemsError[0];
             const topGap = window.pageYOffset + firstItem.getBoundingClientRect().top;
-            window.scrollTo({
-                top: topGap - 55 - 16,
-                behavior: 'smooth'
-            })
-        }
-    }
-    if (faq) {
-        const content = faq.querySelector('.simplebar-content');
-        faq.addEventListener('click', (e) => {
-            const target = e.target;
-            const tag = target.closest('[data-chat-tag-id]');
-            if (!tag) return;
-            const currentID = tag.dataset.chatTagId;
-            const currentFaq = dataFaq[currentID];
-            if (currentFaq) {
-                content.insertAdjacentHTML('beforeend', generateMessageMe(currentFaq.question));
-                content.insertAdjacentHTML('beforeend', generateMessageAi(currentFaq.answer));
-                scrollContent();
-                if (!faq.classList.contains('_active')) faq.classList.add('_active');
-                scrollToBlock();
-            }
-        })
-
-
-        function generateMessageMe(text) {
-            const html = `
-            <div class="message-item chat__message chat__message--me">
-                <div class="message-item__text">
-                    <p>
-                       ${text}
-                    </p>
-                    <span>${getCurrentTime()}</span>
-                </div>
-            </div>
-            `;
-            return html;
-        }
-
-        function generateMessageAi(text) {
-            const html = `
-            <div class="message-item chat__message">
-                <div class="message-item__text">
-                    ${text}
-                    <span>${getCurrentTime()}</span>
-                </div>
-            </div>
-            `;
-            return html;
-        }
-        function scrollContent() {
-            const element = faq.querySelector('.simplebar-content-wrapper');
-            const content = element.querySelector('.simplebar-content');
-            const lastMessageMe = element.querySelector('.message-item:nth-last-child(2)');
-            const lastMessage = element.querySelector('.message-item:last-child');
-            element.scrollTo({
-                top: content.clientHeight - lastMessageMe.clientHeight - lastMessage.clientHeight - 24,
-            })
-        }
-        function scrollToBlock() {
-            const topGap = window.pageYOffset + faq.getBoundingClientRect().top;
             window.scrollTo({
                 top: topGap - 55 - 16,
                 behavior: 'smooth'
--- src/js/mortgage-insur.js
+++ src/js/mortgage-insur.js
@@ -8,25 +8,95 @@ import './_main-scripts';
 import datePickers from './components/datePickers'
 import setCurrentDate from './modules/setCurrentDate';
 import { _slideDown,_slideUp } from './support-modules/slide';
+import { getCurrentTime } from './modules/date';
+import dataFaq from './data/dataFaq';
 // ==============================
 
 document.addEventListener('DOMContentLoaded', () => {
     datePickers();
     setCurrentDate();
     const form = document.querySelector('.mortgage-insur__form');
-    if (!form) return;
-    const bankFilter = form.querySelector('.mortgage-insur__bank');
-    const itemsToggle = form.querySelectorAll('.mortgage-insur__item-hidden');
-    bankFilter.addEventListener('click', () => {
-        const itemActive = bankFilter.querySelector('.search-select-one__item._active');
-        if (itemActive) {
-            if (!itemActive.hasAttribute('data-bank-second')) {
-                itemsToggle.forEach(item => item.setAttribute('hidden', ''));
-            } else {
-                itemsToggle.forEach(item => item.removeAttribute('hidden'));
+    const faq = document.querySelector('.create-policy__faq');
+    if (form) {
+        const bankFilter = form.querySelector('.mortgage-insur__bank');
+        const itemsToggle = form.querySelectorAll('.mortgage-insur__item-hidden');
+        bankFilter.addEventListener('click', () => {
+            const itemActive = bankFilter.querySelector('.search-select-one__item._active');
+            if (itemActive) {
+                if (!itemActive.hasAttribute('data-bank-second')) {
+                    itemsToggle.forEach(item => item.setAttribute('hidden', ''));
+                } else {
+                    itemsToggle.forEach(item => item.removeAttribute('hidden'));
+                }
             }
+        });
+    }
+    if (faq) {
+        const content = faq.querySelector('.simplebar-content');
+        faq.addEventListener('click', (e) => {
+            const target = e.target;
+            const tag = target.closest('[data-chat-tag-id]');
+            if (!tag) return;
+            const currentID = tag.dataset.chatTagId;
+            const currentFaq = dataFaq[currentID];
+            if (currentFaq) {
+                content.insertAdjacentHTML('beforeend', generateMessageMe(currentFaq.question));
+                content.insertAdjacentHTML('beforeend', generateMessageAi(currentFaq.answer));
+                scrollContent();
+                if (!faq.classList.contains('_active')) faq.classList.add('_active');
+                scrollToBlock();
+                hiddenCurrentFaq(tag);
+            }
+        })
+
+
+        function generateMessageMe(text) {
+            const html = `
+            <div class="message-item chat__message chat__message--me">
+                <div class="message-item__text">
+                    <p>
+                       ${text}
+                    </p>
+                    <span>${getCurrentTime()}</span>
+                </div>
+            </div>
+            `;
+            return html;
         }
-    });
+
+        function generateMessageAi(text) {
+            const html = `
+            <div class="message-item chat__message">
+                <div class="message-item__text">
+                    ${text}
+                    <span>${getCurrentTime()}</span>
+                </div>
+            </div>
+            `;
+            return html;
+        }
+        function scrollContent() {
+            const element = faq.querySelector('.simplebar-content-wrapper');
+            const content = element.querySelector('.simplebar-content');
+            const lastMessageMe = element.querySelector('.message-item:nth-last-child(2)');
+            const lastMessage = element.querySelector('.message-item:last-child');
+            element.scrollTo({
+                top: content.clientHeight - lastMessageMe.clientHeight - lastMessage.clientHeight - 24,
+            })
+        }
+        function scrollToBlock() {
+            const topGap = window.pageYOffset + faq.getBoundingClientRect().top;
+            window.scrollTo({
+                top: topGap - 55 - 16,
+                behavior: 'smooth'
+            })
+        }
+
+        function hiddenCurrentFaq(target) {
+            if (!target) return;
+            target.remove();
+        }
+    }
 
 
     document.addEventListener('click', (e) => {

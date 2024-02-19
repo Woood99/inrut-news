47f0a58fdda6ab80ab402638e17bb5a603370db2
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2024-02-19 13:57:32 +0300

    fix

--- src/js/components/calendar.js
+++ src/js/components/calendar.js
@@ -181,7 +181,7 @@ export const calendarSecondary = (containerSelector, eventsSelector, edit = fals
         eventContent: (obj) => {
             return {
                 html: `
-            <span>${obj.event._def.extendedProps.time}</span>
+            <span>${obj.event._def.extendedProps.timeStart}</span>
             <span>${obj.event._def.title}</span>
             `
             }
@@ -233,61 +233,84 @@ export const calendarSecondary = (containerSelector, eventsSelector, edit = fals
             </div>
             `;
             modal(modalHTML, '.calendar-event', 300);
-            let editHTML = '';
-            if (edit) {
-                editHTML = `
-                <div class="calendar-event-item__bottom">
-                    <button type="button" class="btn btn-reset calendar-event-item__cancel">
-                        <svg>
-                            <use xlink:href="./img/sprite.svg#x"></use>
-                        </svg>
-                        Отменить
-                    </button>
-                    <button type="button" class="btn btn-reset calendar-event-item__edit">
-                        <svg>
-                            <use xlink:href="./img/sprite.svg#pencil"></use>
-                        </svg>
-                        Редактировать
-                    </button>
-                </div>
-                `;
-            }
             eventsArray.forEach(el => {
                 if (el.date === eventDate) {
-                    const itemHTML = `
-                <li class="calendar-event__item calendar-event-item">
-                    <div class="calendar-event-item__time">
-                        <svg>
-                            <use xlink:href="./img/sprite.svg#clock"></use>
-                        </svg>
-                        <div>
-                            <span>${el.time}</span>
-                            <span>${el.date}</span>
-                        </div>
-                    </div>
-                    <div class="calendar-event-item__location">
-                        <svg>
-                            <use xlink:href="./img/sprite.svg#location"></use>
-                        </svg>
-                        ${el.location}
-                    </div>
-                        <div class="calendar-event-item__title">
-                            ${el.title}
-                        </div>
-                        <span class="calendar-event-item__price">${numberReplace(`${el.price}`)} ₽</span>
-                        <div class="calendar-event-item__user user-info">
+                    console.log(el);
+                    let itemUsersHTML = '';
+                    let itemNotifHTML = '';
+                    el.participants.forEach(user => {
+                        itemUsersHTML += `
+                        <div class="event__user user-info user-info--small">
                             <div class="user-info__avatar avatar">
-                                <img loading="lazy" src="${el.user.avatar}" width="32" height="32" alt="${el.user.name}">
+                                <img loading="lazy" src="${user.avatar}" width="30" height="30" alt="${user.name}">
                             </div>
                             <span class="user-info__name">
-                                ${el.user.name}
-                            </span>
-                            <span class="user-info__pos">
-                                ${el.user.pos}
+                                ${user.name}
                             </span>
                         </div>
-                        ${editHTML}
-                </li>
+                        `;
+                    });
+                    el.notifications.forEach(notif => {
+                        itemNotifHTML += `
+                        <div class="event__notif">
+                            <span>${notif.time}</span>
+                            <span>${notif.method}</span>
+                        </div>
+                        `;
+                    });
+                    const itemHTML = `
+                        <li class="calendar-event__item event">
+                            <div class="event__header">
+                                <button type="button" class="btn btn-reset event__edit">
+                                    <svg>
+                                        <use xlink:href="./img/sprite.svg#pencil">
+                                        </use>
+                                    </svg>
+                                </button>
+                                <button type="button" class="btn btn-reset event__remove">
+                                    <svg>
+                                        <use xlink:href="./img/sprite.svg#trash">
+                                        </use>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div class="event__title">
+                                ${el.title}
+                            </div>
+                            <div class="event__descr">
+                                ${el.descr}
+                            </div>
+                            <div class="event__time">
+                                <span>${el.date}</span>
+                                <span>${el.timeStart}</span>
+                                <span>一</span>
+                                <span>${el.timeEnd}</span>
+                            </div>
+                            <div class="event__users">
+                                <h4 class="title-4 event__subtitle">Участники</h4>
+                                ${itemUsersHTML}
+                            </div>
+                            <div class="event__location">
+                                <h4 class="title-4 event__subtitle">Адрес или место</h4>
+                                <span>${el.location}</span>
+                            </div>
+                            <div class="event__link">
+                                <h4 class="title-4 event__subtitle">Ссылка на звонок</h4>
+                                <a href="${el.link}">${el.link}</a>
+                            </div>
+                            <div class="event__files">
+                                <h4 class="title-4 event__subtitle">Файлы</h4>
+                                <a href="${el.link}">${el.link}</a>
+                            </div>
+                            
+                            <div class="event__notifs">
+                                <h4 class="title-4 event__subtitle">Уведомления</h4>
+                                ${itemNotifHTML}
+                            </div>
+                            <div class="event__status">
+                                ${el.status}
+                            </div>
+                        </li>
                     `;
                     document.querySelector('.calendar-event__list').insertAdjacentHTML('beforeend', itemHTML);
                 }

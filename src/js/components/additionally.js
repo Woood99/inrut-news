f549db48d409e098cac011a8d4663bb3a9b213e6
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2024-03-07 12:19:02 +0300

    fix

--- src/js/components/additionally.js
+++ src/js/components/additionally.js
@@ -6,6 +6,7 @@ const additionally = () => {
     const containers = document.querySelectorAll('.additionally');
     if (containers.length === 0) return;
     containers.forEach(container => {
+        init(container.querySelectorAll('[data-additionally-card-calc]'));
         const moreBtn = container.querySelector('.additionally__more');
         const moreBtnText = moreBtn.querySelector('span');
         const moreBtnTextDefault = moreBtnText.textContent;
@@ -42,9 +43,11 @@ const additionally = () => {
                     if (target.checked) {
                         card.classList.add('_active');
                         totalSumm -= currentSumm;
+                        sendingToBasket(card);
                     } else {
                         card.classList.remove('_active');
                         totalSumm += currentSumm;
+                        removeBasketFromCard(card);
                     }
                     updateDescr();
                     checkErrorCardsSumm(container, totalSumm);
@@ -65,6 +68,22 @@ const additionally = () => {
                 })
                 unsuitableCards.forEach(card => card.classList.add('_disabled-opacity'));
             }
+            document.addEventListener('click', (e) => {
+                const target = e.target;
+                const removeCard = target.closest('.card-checkbox__remove');
+                if (removeCard) {
+                    const card = removeCard.closest('.card-checkbox');
+                    if (card) {
+                        const currentID = card.dataset.cardBasketIndex;
+                        const currentCard = document.querySelector(`[data-card-additionally-index='${currentID}']`);
+                        const currentSumm = Number(replaceValue(currentCard.querySelector('[data-card-summ]').textContent));
+                        totalSumm += currentSumm;
+                        removeBasketFromBasket(card,currentCard);
+                        updateDescr();
+                        checkErrorCardsSumm(container, totalSumm);
+                    }
+                }
+            })
         }
         if (typeQuantity) {
             const descr = container.querySelector('.additionally__descr');
@@ -82,10 +101,12 @@ const additionally = () => {
                         card.classList.add('_active');
                         totalSumm += currentSumm;
                         currentQuantity++;
+                        sendingToBasket(card);
                     } else {
                         card.classList.remove('_active');
                         totalSumm -= currentSumm;
                         currentQuantity--;
+                        removeBasketFromCard(card);
                     }
                     const cards = Array.from(container.querySelectorAll('[data-additionally-card-calc]'));
                     cards.forEach(card => card.classList.remove('_disabled-opacity'));
@@ -117,13 +138,97 @@ const additionally = () => {
                     descr.textContent = `Выбрано ${currentQuantity} из ${maxQuantity} подарка`;
                 }
             }
-
+            document.addEventListener('click', (e) => {
+                const target = e.target;
+                const removeCard = target.closest('.card-checkbox__remove');
+                if (removeCard) {
+                    const card = removeCard.closest('.card-checkbox');
+                    if (card) {
+                        const currentID = card.dataset.cardBasketIndex;
+                        const currentCard = document.querySelector(`[data-card-additionally-index='${currentID}']`);
+                        const currentSumm = Number(replaceValue(currentCard.querySelector('[data-card-summ]').textContent));
+                        totalSumm -= currentSumm;
+                        currentQuantity--;
+                        removeBasketFromBasket(card,currentCard);
+                        const cards = Array.from(container.querySelectorAll('[data-additionally-card-calc]'));
+                        cards.forEach(card => card.classList.remove('_disabled-opacity'));
+                        updateDescr();
+                        checkErrorCardsSumm(cards, totalSumm, maxSumm);
+                        checkErrorCardsQuantity(cards);
+                    }
+                }
+            })
         }
 
         function replaceValue(el) {
             return el.replace(/[^0-9]/g, '');
         }
     })
+    function init(items) {
+        items.forEach((item, index) => item.setAttribute('data-card-additionally-index', index));
+    }
+
+    function sendingToBasket(card) {
+        const basketContainer = document.querySelector('.additionally-calc');
+        if (!basketContainer) return;
+        const basket = basketContainer.querySelector('.additionally-calc__list .simplebar-content');
+        if (!basket) return;
+        const cardMap = {
+            index: card.dataset.cardAdditionallyIndex,
+            title: card.querySelector('.user-info__name').textContent.trim(),
+            descr: card.querySelector('.user-info__pos').textContent.trim(),
+            oldPrice: card.querySelector('.card-checkbox__price span:nth-child(1)').textContent.trim(),
+            newPrice: card.querySelector('.card-checkbox__price span:nth-child(2)').textContent.trim(),
+        };
+        const cardHTML = `
+        <li class="card-checkbox" data-card-basket-index="${cardMap.index}">
+            <div class="user-info card-checkbox__info">
+                <span class="user-info__name">
+                    ${cardMap.title}
+                </span>
+                <span class="user-info__pos">
+                    ${cardMap.descr}
+                </span>
+            </div>
+            <div class="card-checkbox__price">
+                <span>${cardMap.oldPrice}</span>
+                <span>${cardMap.newPrice}</span>
+            </div>
+            <button type="button" class="btn btn-reset btn-remove card-checkbox__remove" title="Удалить">
+                <svg>
+                    <use xlink:href="./img/sprite.svg#trash">
+                    </use>
+                </svg>
+            </button>
+        </li>
+        `;
+        basket.insertAdjacentHTML('beforeend', cardHTML);
+        visibleOrHiddenBasket();
+    }
+
+    function removeBasketFromBasket(card,currentCard) {
+        currentCard.classList.remove('_active');
+        currentCard.querySelector('input').checked = false;
+        card.remove();
+        visibleOrHiddenBasket();
+    }
+    function removeBasketFromCard(card){
+        const currentID = card.dataset.cardAdditionallyIndex;
+        const currentCard = document.querySelector(`[data-card-basket-index='${currentID}']`);
+        currentCard.remove();
+        visibleOrHiddenBasket();
+    }
+    function visibleOrHiddenBasket(){
+        const basketContainer = document.querySelector('.additionally-calc');
+        if (!basketContainer) return;
+        const basket = basketContainer.querySelector('.additionally-calc__list .simplebar-content');
+        if (!basket) return;
+        if (basket.children.length === 0){
+            basketContainer.setAttribute('hidden','');
+        } else {
+            basketContainer.removeAttribute('hidden');
+        }
+    }
 }
 
 export default additionally;

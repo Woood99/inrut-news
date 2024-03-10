601c0205af0941ba7abba95a45937b9bea6e9e6e
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2024-03-10 13:32:10 +0300

    fix

---
+++ src/js/components/decreasingTime.js
@@ -0,0 +1,12 @@
+const decreasingTime = () => {
+    const containers = document.querySelectorAll('[data-decreasing-time]');
+    if (containers.length === 0) return;
+    containers.forEach(container => {
+        init(container);
+    });
+
+    function init(container) {
+        const start = container.dataset.decreasingTimeStart;
+        console.log(start);
+    }
+};
